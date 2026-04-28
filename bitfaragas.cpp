5b1cad1144cf010ad8d4df506f85e071b964269e
Gellert15 <99840385+Gellert15@users.noreply.github.com>
2026-04-28 09:41:13 +0300

    bitfaragas.cpp

--- bitfaragas.cpp
+++ bitfaragas.cpp
@@ -1,2 +1,2 @@
 #include<bits/stdc++.h>
-using namespace std;int main(){string t;getline(cin,t);string y(t.rbegin(),t.rend());cout<<(t==y?"Igen\n":"Nem\n");int n;cin>>n;int a[n];for(int&v:a)cin>>v;sort(a,a+n);for(int v:a)cout<<v<<" ";}
+using namespace std;string t;int main(){int n;cin>>t>>n;int a[n];puts(t==string(t.rbegin(),t.rend())?"Y":"N");for(int&v:a)cin>>v;sort(a,a+n);for(int v:a)cout<<v<<' ';}
